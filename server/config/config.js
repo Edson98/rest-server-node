920d2c46a0ddd46366d2c343affad7abe2a1fa55
Edson98 <edsoncoazaca@upeu.edu.pe>
2020-04-02 09:47:58 -0500

    Token y login

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,29 @@
 // =====================
 // Puerto
 // =====================
-
 process.env.PORT = process.env.PORT || 3000;
+
+// =====================
+// Entorno
+// =====================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =====================
+// Vencimiento del token
+// =====================
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// =====================
+// SEED de autenticación
+// =====================
+process.env.SEED = process.env.SEED || 'secret';
+
+// =====================
+// Database
+// =====================
+let urlDB;
+if ( process.env.NODE_ENV === 'dev' ) {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://edson:0jtRA8Z6qIWvvl7L@cluster0-ii2fv.mongodb.net/cafe';
+}
--- server/server.js
+++ server/server.js
@@ -1,39 +1,29 @@
-require('./config/config')
+require('./config/config');
 
-const express = require('express')
-const app = express()
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
- 
+ // importacion de rutas
+app.use(require('./routes/index'));
 
-app.get('/usuario', function (req, res) {
-    res.json('get usuario')
-});
- 
-app.post('/usuario', function (req, res) {
-    let body = req.body;
-
-    if ( body.nombre === undefined ) {
-        res.status(400).json({
-            ok: false,
-            mensaje:  'El nombre es necesario'
-        });
-    }
-
-    res.json({ persona : body })
+mongoose.connect('mongodb://localhost:27017/cafe',
+    {   useNewUrlParser:    true, 
+        useUnifiedTopology: true,
+        useCreateIndex:     true   },
+    (err, res) =>{
+        if (err) throw err;
+        console.log('Base de datos ONLINE');
 });
 
-app.put('/usuario', function (req, res) {
-    res.json('put usuario')
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('get usuario')
-});
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', 3000);  
 });
